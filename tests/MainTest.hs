737ce45d53a196f36c312781e8c6e2a39f22f29d
leticialunaz <leticialunadb@gmail.com>
2026-01-18 02:47:26 -0300

    correcao para comando cabal test

---
+++ tests/MainTest.hs
@@ -0,0 +1,14 @@
+module Main where
+
+import Test.HUnit
+import BoardTests (tests_Board)
+import GeneratorTests (tests_Generator)
+
+main :: IO ()
+main = do
+    putStrLn "==== Rodando testes Board ===="
+    _ <- runTestTT tests_Board
+    putStrLn "==== Rodando testes Generator ===="
+    _ <- runTestTT tests_Generator
+
+    return ()
