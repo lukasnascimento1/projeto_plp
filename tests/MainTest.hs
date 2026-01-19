3960895ba9253491940c179a76626d2ae1d2477e
Alana <alanandrade.vanessa@gmail.com>
2026-01-19 00:51:06 -0300

    incluindo testes de validação

--- tests/MainTest.hs
+++ tests/MainTest.hs
@@ -3,6 +3,7 @@ module Main where
 import Test.HUnit
 import BoardTests (tests_Board)
 import GeneratorTests (tests_Generator)
+import ValidationTests (tests_Validation)
 
 main :: IO ()
 main = do
@@ -10,5 +11,7 @@ main = do
     _ <- runTestTT tests_Board
     putStrLn "==== Rodando testes Generator ===="
     _ <- runTestTT tests_Generator
+    putStrLn "==== Rodando testes Validation ===="
+    _ <- runTestTT tests_Validation
 
     return ()
